4f2c33a5d13264ede285901752617f6872397602
Vincent Nguyen <vpvnguyen@gmail.com>
2019-08-21 23:23:19 -0700

    fixed server merge

--- server.js
+++ server.js
@@ -7,20 +7,6 @@ const uuid = require('node-uuid');
 
 let PORT = process.env.PORT || 3000;
 
-<<<<<<< HEAD
-// public
-app.use(express.static('./app/view/public'));
-
-// routing; when a dir is required, it will look for the index.js; index.js exports router
-app.use(require('./app/routes'));
-
-// middleware
-router.use(function timeLog(req, res, next) {
-    console.log('Time: ', Date.now());
-    next();
-});
-
-=======
 // start logging
 morgan.token('id', function getId(req) {
     return req.id
@@ -51,5 +37,4 @@ function assignId(req, res, next) {
     next();
 }
 
->>>>>>> 1f37b12b98698b11081a74736188eadbe060bfb5
 app.listen(3000, () => { console.log(`App is listening on PORT: ${PORT}`) });
